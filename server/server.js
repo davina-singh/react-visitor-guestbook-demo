eeb76fe7b5ed99d9288ec895da75d3d6aa29031b
DAVINA SINGH <davina_singh@outlook.com>
2024-06-12 10:43:12 +0100

    initial

---
+++ server/server.js
@@ -0,0 +1,37 @@
+import express from "express";
+import Database from "better-sqlite3";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+const db = new Database("bigger.db");
+const PORT = process.env.PORT || 3000;
+app.use(cors());
+app.use(express.json());
+
+app.get("/messages", (req, res) => {
+  // get the query string filter value from the request
+  const filter = req.query.filter;
+  let messages = [];
+  if (!filter || filter === "all") {
+    messages = db.prepare("SELECT * FROM messages").all();
+  } else {
+    messages = db
+      .prepare("SELECT * FROM messages WHERE reaction = ?")
+      .all(filter);
+  }
+
+  res.json(messages);
+});
+
+app.post("/messages", function (req, res) {
+  db.prepare(
+    "INSERT INTO messages (username, content, reaction) VALUES (?, ?, ?)"
+  ).run(req.body.username, req.body.content, req.body.reaction);
+  res.json({ success: true });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started on PORT ${PORT}`);
+});
